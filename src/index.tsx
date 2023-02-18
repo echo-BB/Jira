d5e0f76fabd2f3ef8b98a5828218f9164e88e846
EchoWang <1223180812@qq.com>
2023-02-18 19:24:11 +0800

    chore: 安装devtool

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Date: 2023-01-15 16:29:13
  * @LastEditors: EchoWang
- * @LastEditTime: 2023-02-07 15:42:00
+ * @LastEditTime: 2023-02-18 17:04:35
  * @FilePath: \Jira\src\index.tsx
  * @Description: 
  */
@@ -10,18 +10,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { loadDevTools } from "jira-dev-tool";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 
-
-
-
-root.render(
+loadDevTools(()=>root.render(
+  <React.StrictMode>
     <App />
-);
+  </React.StrictMode>
+))
+
+
+
 
 
 // If you want to start measuring performance in your app, pass a function
